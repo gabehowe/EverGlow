322bc664402e4368196ce8a6940a34939bac84fe
Midnightfury <31964173+MIdnightfury@users.noreply.github.com>
2020-12-25 18:23:35 -0500

    Update EverGlow.java

--- main/java/io/github/midnightfury/EverGlow.java
+++ main/java/io/github/midnightfury/EverGlow.java
@@ -7,7 +7,6 @@ public final class EverGlow extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new Listeners(), this);
         this.getCommand("glow").setExecutor(new GlowCommand());
         this.getCommand("glow").setTabCompleter(new GlowCommand());
 
